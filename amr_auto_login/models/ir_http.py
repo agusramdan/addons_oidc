8610860ec0572eb3e798cee84b4a60e70996bb32
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-06-01 01:22:19 +0700

    amr_auto_login

--- amr_auto_login/models/ir_http.py
+++ amr_auto_login/models/ir_http.py
@@ -10,34 +10,6 @@ class IrHttp(models.AbstractModel):
     _inherit = 'ir.http'
 
     @classmethod
-    def set_session(cls, user, session_token=None):
-        session = request.session
-        session.rotate = True
-        session.uid = user.id
-        session.login = user.login
-        if user:
-            session.session_token = security.compute_session_token(session, request.env)
-        else:
-            session.session_token = session_token
-        #v16 handle session berbeda dengan 16
-        # if not session.session_token:
-        #     request.update_env()
-        #     session.uid = None
-        #     session.login = None
-        # else:
-        #     request.update_env(user=request.session.uid)
-
-        # v13 handle session berbeda dengan 16
-        if not session.session_token:
-            request.uid = None
-            session.uid = None
-            session.login = None
-        else:
-            request.uid = user.id
-            request.disable_db = False
-            session.get_context()
-
-    @classmethod
     def _auth_method_auto_login(cls):
         # Menggunakan token oidc untuk handle API call
         heleper = request.env['amr.resource.helper'].sudo()
