b755581e5edd072cb5820912f5c9e502d160bef6
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-06-07 11:52:44 +0700

    amr_resource 13.0.0.0.2

--- amr_resource/controllers/main.py
+++ amr_resource/controllers/main.py
@@ -2,6 +2,8 @@
 
 from werkzeug.utils import redirect
 from odoo.http import Controller, request, route
+from odoo.api import call_kw
+from odoo.models import check_method_name
 
 
 class RedirectController(Controller):
@@ -10,3 +12,14 @@ class RedirectController(Controller):
     def redirect_url(self, url=None):
         url = request.env.user.add_url_access_token(url)
         return redirect(url)
+
+
+class JsonRpcApi(Controller):
+
+    def _call_kw(self, model, method, args, kwargs):
+        check_method_name(method)
+        return call_kw(request.env[model], method, args, kwargs)
+
+    @route('/api/dataset/call_kw', type='json', auth="machine")
+    def call_kw(self, model, method, args, kwargs):
+        return self._call_kw(model, method, args, kwargs)
