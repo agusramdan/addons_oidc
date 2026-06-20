837407433d0089dff5eb195751b85fa2be1256d6
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-06-20 16:40:19 +0700

    amr_resource 13.0.0.0.5

--- amr_resource/controllers/main.py
+++ amr_resource/controllers/main.py
@@ -13,6 +13,29 @@ class RedirectController(Controller):
         url = request.env.user.add_url_access_token(url)
         return redirect(url)
 
+    @route(['/api/v1/access/url'], type='http', auth='user_or_param', methods=['GET'], csrf=False)
+    def access_url(self, **kwargs):
+        data = dict(kwargs)
+        res_id = data.get("res_id", None)
+        res_model = data.get("res_model", None)
+
+        model_object = request.env[res_model].browse(res_id)
+        if callable(getattr(model_object, 'get_url_access_token', None)):
+            result = model_object.get_url_access_token()
+        else:
+            type_url = data.get("type_url", "internal")
+            if type_url == 'internal':
+                url = model_object.get_internal_url()
+            else:
+                url = model_object.get_public_url()
+
+            access_url = request.env.user.add_url_access_token(url)
+            result = {
+                'url': url,
+                'access_url': access_url,
+            }
+        return redirect(url)
+
 
 class JsonRpcApi(Controller):
 
