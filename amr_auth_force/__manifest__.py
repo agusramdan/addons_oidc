7531a94b20094a56ce16c4eacdc74324e68bdd78
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-06-28 22:56:57 +0700

    amr_auth_force 13.0.1.0.0

--- amr_auth_force/__manifest__.py
+++ amr_auth_force/__manifest__.py
@@ -1,11 +1,11 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
+# -*- coding: utf-8 -*-
 
 {
     'name': 'Authentication Force',
     'depends': ['base', 'base_setup', 'auth_oauth' ],
     #'description': < auto-loaded from README file
     'category': 'Tools',
-    'version': '16.0.0.0.0',
+    'version': '13.0.1.0.0',
     'depends': ['base', 'web', 'base_setup', 'auth_oauth'],
     'data': [
         'views/auth_oauth_provider_views.xml',
