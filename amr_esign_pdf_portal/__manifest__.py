0444810dd3d3cfcb0fd9b3cf0190163af9fb1729
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2026-07-12 16:49:44 +0700

    amr_esign_pdf_portal 13.0.1.0.0

---
+++ amr_esign_pdf_portal/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'ESign PDF Portal',
+    'version': '13.0.1.0.0',
+    'category': 'Tools',
+    'summary': 'Manage PAdES multiple signature PDF workflow in Odoo 13.',
+    'description': 'Support PDF document preparation, CMS request, and multiple signature workflow for Odoo 13.',
+    'author': 'Auto Generated',
+    'website': 'https://example.com',
+    'license': 'LGPL-3',
+    'depends': ['base', 'web', 'amr_esign_pdf', 'amr_pdf_viewer'],
+    'data': [
+        'views/pdf_sign_portal_templates.xml',
+    ],
+    'demo': [],
+    'assets': {},
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
